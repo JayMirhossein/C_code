707fc9bfc0d074a11c2fdfd68a2ec82bbe3de7d5
JayMirhossein <mirhossein.jalali@gmail.com>
2023-12-23 10:19:58 +0800

    Create input.c
    add c code

---
+++ input.c
@@ -0,0 +1,12 @@
+// C code to read inoput 
+
+#include <stdio.h>
+
+int main() {
+    int num;
+    float fnum;
+    printf("Enter an integer and a floating-point number: ");
+    scanf("%d %f", &num, &fnum);
+    printf("You entered %d and %f\n", num, fnum);
+    return 0;
+}
